1c9403b190fdfb61f81d2bb111574481b188e9b9
franklinchenw <franklinchenw@gmail.com>
2025-03-10 17:34:45 -0400

    update next configure

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["antd"],
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "upgrade-insecure-requests; default-src * 'unsafe-inline' 'unsafe-eval'; img-src * data: blob: 'unsafe-inline'",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
